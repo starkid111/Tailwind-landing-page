98ad9fa4c01c5fb9f6b430d9572a670534a0b585
Ramadan <Ramadanadex111@gmail.com>
2024-01-13 20:48:21 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './index.html',
+    './script.js',
+    './src/**/*.{html,js}',
+  ],
   theme: {
     screens : {
       sm: '480px',
